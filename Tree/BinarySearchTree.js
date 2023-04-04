7b4fc62a80db059f4062e810c904b415ca4d8800
Keyur Mahajan <keyur.mahajan24@gmail.com>
2023-04-04 08:00:39 +0530

    Remove Node Implementation

--- Tree/BinarySearchTree.js
+++ Tree/BinarySearchTree.js
@@ -62,19 +62,87 @@ class Node {
       }
     }
     // remove
+
+    remove(value){
+      if(this.root === null){
+        return null;
+      }
+      let current = this.root;
+      
+      let prev = null;
+      while(current.value !== value){
+        
+        if(current.value > value){
+           if(current.left !== null){
+            prev = current;
+             current = current.left;
+           }else{
+            return null;
+           }       
+        }else{
+          if(current.right !== null){
+            prev = current;
+             current = current.right;
+           }else{
+             return null;
+           }       
+        }
+      }
+      let lFlag = current.left === null;
+      let rFlag = current.right === null;
+
+     if(!lFlag && !rFlag){
+       let newCurrent = current.right ;
+       let newCurrentPrev = null;
+       while(newCurrent.left){
+        newCurrentPrev = newCurrent;
+        newCurrent = newCurrent.left;
+       }
+       if(newCurrentPrev){
+         current.value = newCurrent.value;
+         newCurrentPrev.left = newCurrent.right; 
+       }else{
+        newCurrent.left =current.left;
+        current = newCurrent;
+       }
+       return this;
+      }else{
+        if(prev.value  > current.value){
+          prev.left = current.left;
+        }else{
+          prev.right =current.right;
+        }
+        return this;
+      }
+
+    }
   }
   
   const tree = new BinarySearchTree();
-  tree.insert(9)
-  tree.insert(4)
-  tree.insert(6)
-  tree.insert(20)
-  tree.insert(170)
-  tree.insert(15)
+  tree.insert(63)
+  tree.insert(61)
+  tree.insert(75)
+  tree.insert(8)
+  tree.insert(62)
+  tree.insert(68)
+  tree.insert(94)
   tree.insert(1)
+  tree.insert(14)
+  tree.insert(89)
+  tree.insert(9)
+  tree.insert(12)
   console.log(JSON.stringify(traverse(tree.root)))
-  console.log(tree.lookup(4))
-  
+  console.log(tree.lookup(94))
+  console.log(tree.remove(8));
+  console.log(JSON.stringify(traverse(tree.root)));
+  console.log(tree.remove(75));
+  console.log(JSON.stringify(traverse(tree.root)));
+  console.log(tree.remove(68));
+  console.log(JSON.stringify(traverse(tree.root)));
+  console.log(tree.remove(89));
+  console.log(JSON.stringify(traverse(tree.root)));
+  console.log(tree.remove(94));
+  console.log(JSON.stringify(traverse(tree.root)))
   //     9
   //  4     20
   //1  6  15  170
